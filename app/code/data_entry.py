8ba4af1c2ba8ae7a3eafb6c03e25570d0d2cb570
kishoredurai <kishore.ct19@bitsathy.ac.in>
2021-06-01 23:43:42 +0530

    session details add and delete

--- app/code/data_entry.py
+++ app/code/data_entry.py
@@ -80,11 +80,49 @@ def data_entry_course_update():
 
 @app.route("/data_entry/session", methods=["POST", "GET"])
 def data_entry_session():
+    cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
     if 'id' in session and session.get("user_type") == 'data_entry':
-        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        if request.method == 'POST':      
+            adminid=session.get('id')
+            cname = request.form['cname']
+            sduration = request.form['sduration']
+            sname = request.form['sname']
+            desc = request.form['desc']
+            sdate = request.form['sdate']
+            stime = request.form['stime']
+            etime = request.form['etime']          
+            fid = request.form['fid']                   
+         
+            try:
+                cursor.execute("INSERT INTO course_session_details (course_id, session_duration ,faculty_id,session_name,session_discription,session_date,session_starttime,session_endtime) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)",[cname,sduration,fid,sname,desc,sdate,stime,etime])
+                mysql.connection.commit()
+                return jsonify('success')
+            except Exception as Ex:
+                return jsonify('error')
+
+        
+        cursor.execute('SELECT * FROM course_details,subject Where course_details.subject_id=subject.subject_id')
+        course = cursor.fetchall()
+        cursor.execute('SELECT * FROM faculty_details')
+        faculty = cursor.fetchall()
         cursor.execute('SELECT * FROM course_session_details,faculty_details,course_details WHERE course_session_details.faculty_id=faculty_details.faculty_id and course_details.course_id=course_session_details.course_id')
         sess = cursor.fetchall()
-        return render_template('data_entry/course session table.html',session=sess)
+        return render_template('data_entry/course session table.html',session=sess,course=course,faculty=faculty)
+    else:
+        return redirect(url_for('login'))
+
+@app.route("/data_entry/session/update", methods=["POST", "GET"])
+def data_entry_session_update():
+    cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+    if 'id' in session and session.get("user_type") == 'data_entry':     
+        if request.method == "POST":
+            if request.form.get("delete"):
+                result = request.form 
+                id = result["delete"]
+                cursor.execute('delete from course_session_details where session_id=%s;', [id])
+                mysql.connection.commit()
+                flash("Deleted ♥️")                                         
+                return redirect(url_for('data_entry_session'))
     else:
         return redirect(url_for('login'))
 
